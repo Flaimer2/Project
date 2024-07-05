5b816f1bd3b46b33c122d044740046f9ef0aeae4
Flaimer2 <63396386+Flaimer2@users.noreply.github.com>
2024-07-05 22:44:58 +0300

    [0.9.0] Update

--- proxy/start.bat
+++ proxy/start.bat
@@ -3,5 +3,5 @@ title Proxy
 path="C:\Program Files\Eclipse Adoptium\jdk-17.0.10.7-hotspot\bin"
 rd /s /q "logs"
 @echo off
-java -Xms2048M -Xmx2048M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar server.jar nogui
+java -Dfile.encoding=UTF-8 -Xms2048M -Xmx2048M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar server.jar nogui
 PAUSE
